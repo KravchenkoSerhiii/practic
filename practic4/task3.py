7969f7719099a098453cb8f3f287e8b0f6d5bd1f
serhii <admin@admin.com>
2025-12-22 14:18:38 +0100

    Added task3 for practic4

---
+++ practic4/task3.py
@@ -0,0 +1,28 @@
+def print_list(lst, message="List:"):
+    print(message, lst)
+
+
+def remove_even_indices(lst):
+    return lst[1::2]
+
+
+def main():
+    try:
+        user_input = input("Type list elements")
+
+        source_list = user_input.split()
+
+        if not source_list:
+            print("List is empty")
+            return
+
+        print_list(source_list, "Initial list")
+        result_list = remove_even_indices(source_list)
+        print_list(result_list, "Updated list")
+
+    except Exception as e:
+        print(f"Something went wrong: {e}")
+
+
+if __name__ == "__main__":
+    main()
